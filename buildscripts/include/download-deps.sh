3adeef593e60cdd59b1336f8e957ba7392984ddc
feitizzz <feitizzz@users.noreply.github.com>
2026-09-11 18:37:01 +0800

    libxml2 源换 GitHub 镜像（gitlab.gnome.org 503 打断构建）

--- buildscripts/include/download-deps.sh
+++ buildscripts/include/download-deps.sh
@@ -10,7 +10,7 @@ mkdir -p deps && cd deps
 [ ! -d mbedtls ] && git clone --depth 1 --branch v$v_mbedtls --recurse-submodules https://github.com/Mbed-TLS/mbedtls.git mbedtls
 
 # libxml2
-[ ! -d libxml2 ] && git clone --depth 1 --branch v$v_libxml2 --recursive https://gitlab.gnome.org/GNOME/libxml2.git libxml2
+[ ! -d libxml2 ] && git clone --depth 1 --branch v$v_libxml2 --recursive https://github.com/GNOME/libxml2.git  # 镜像：gitlab.gnome.org 偶发 503 libxml2
 
 # ffmpeg
 [ ! -d ffmpeg ] && git clone --depth 1 --branch n$v_ffmpeg https://github.com/FFmpeg/FFmpeg.git ffmpeg
